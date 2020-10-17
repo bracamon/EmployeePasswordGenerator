674aee8731ad4842b62ec68c8ec1b358048bca34
bracamon <bracamontes.ochoa@gmail.com>
2020-10-17 00:44:56 -0700

    separated git status git status git add * git status git push git add *

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,29 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
-var letterUp = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var letterLow = ["abcdefghijklmnopqrstuvwxyz"]
-var number = ["0123456789"]
-var special = ["!@#$%^&*()"]
-var emojis = ["😃❤️🥰😂😅😙👀🤩😎😍🥳😱"]
+var letterUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var letterUpS = letterUp.split('') // creates array of objects out of string
+
+var letterLow = 'abcdefghijklmnopqrstuvwxyz'
+var letterLowS = letterLow.split('')
+
+var number = '0123456789'
+var numberS = number.split('')
+
+var special = '!@#$%^&*()'
+var specialS = special.split('')
+
+var emoji = '😃❤️🥰😂😅😙👀🤩😎😍'
+var emojiS = emoji.split('')
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   
+
+
 
   passwordText.value = password;
 
