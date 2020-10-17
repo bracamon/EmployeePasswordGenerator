3e70c4e05a9714de36edac44fd523c2dd20f3f01
bracamon <bracamontes.ochoa@gmail.com>
2020-10-16 23:32:25 -0700

    Established variable array values for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+var letterUp = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var letterLow = ["abcdefghijklmnopqrstuvwxyz"]
+var number = ["0123456789"]
+var special = ["!@#$%^&*()"]
+var emojis = ["😃❤️🥰😂😅😙👀🤩😎😍🥳😱"]
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
@@ -12,3 +19,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
