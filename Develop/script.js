6bc1d4108ae7f35630bd7a0b3628f64388b643b4
bracamon <bracamontes.ochoa@gmail.com>
2020-10-17 00:47:31 -0700

    Created prompt for length of password. Added alert for value outside of accepted range

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,14 @@ var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 var letterUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var letterUpS = letterUp.split('') // creates array of objects out of string
+var letterUpS = letterUp.split('') // creates array of individual values out of continuous string
 
 var letterLow = 'abcdefghijklmnopqrstuvwxyz'
 var letterLowS = letterLow.split('')
 
 var number = '0123456789'
 var numberS = number.split('')
+console.log(numberS)
 
 var special = '!@#$%^&*()'
 var specialS = special.split('')
@@ -23,12 +24,20 @@ var emojiS = emoji.split('')
 function writePassword() {
   var password = generatePassword();
   
+  
 
 
 
   passwordText.value = password;
 
 }
+
+var length = prompt("Enter number between 8-128 for password length")
+if (length < 8 || length > 128) {
+  alert("Please enter a number between 8-128")
+} 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
