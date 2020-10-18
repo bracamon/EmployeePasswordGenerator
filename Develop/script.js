442261a02e47f2401cb7fa1dab226002b76a4860
bracamon <bracamontes.ochoa@gmail.com>
2020-10-17 22:31:38 -0700

    Updated language in confirm popups

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var number = [0,1,2,3,4,5,6,7,8,9]
 var special = '!@#$%^&*()'
 var specialS = special.split('')
 
-var emoji = ["⭐️","❤️","🥰","😂","😅","😙","👀","🤩","😎","😍"];
+var emoji = ["⭐️","❤️","🥰","😂","😅","😙","👀","🤩","😎","😍","🍉","🍇","🍎","🍑","🥑","🍕","🥞","🧀","🥭"];
 
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
@@ -23,11 +23,11 @@ function writePassword() {
     alert("Please enter a number between 8-128");
   } else {
 
-    var letterUpSTrue = confirm("Do you want to use uppercase letters?");
-    var letterLowSTrue = confirm("Do you want to use lowercase letters?");
-    var numberTrue = confirm("Do you want to use numbers");
-    var specialSTrue = confirm("Do you want to use special characters?");
-    var emojiTrue = confirm("Do you want to use emojis?");
+    var letterUpSTrue = confirm("Click 'OK' to include uppercase letters?");
+    var letterLowSTrue = confirm("Click 'OK' to include lowercase letters?");
+    var numberTrue = confirm("Click 'OK' to include numbers");
+    var specialSTrue = confirm("Click 'OK' to include special characters?");
+    var emojiTrue = confirm("Click 'OK' to include emojis?");
 
     if (letterUpSTrue) {possibles.push(letterUpS)};
     if (letterLowSTrue) {possibles.push(letterLowS)};
